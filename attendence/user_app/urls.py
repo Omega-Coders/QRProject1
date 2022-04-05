bd5086c97b2b6bb8ded97e975a2cf90bf9302d98
manikanta700 <20pa1a5455@vishnu.edu.in>
2022-04-05 11:42:15 +0530

    version-5

--- attendence/user_app/urls.py
+++ attendence/user_app/urls.py
@@ -3,5 +3,10 @@ from . import views
 urlpatterns = [
     path('register/', views.register1),
     path('register/register', views.register),
+    path('login/', views.login1),
+    path('login/login', views.login),
+    path('register/login', views.login),
+
+
     
 ]
--- attendence/user_app/views.py
+++ attendence/user_app/views.py
@@ -1,10 +1,14 @@
 
+from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Teacher, Department, Attendence
 
 def register1(request):
-    return render(request, "home.html")
+    return render(request, "reg_form.html")
+
+def login1(request):
+    return render(request,'login.html')
 
 def register(request):
 
@@ -17,13 +21,38 @@ def register(request):
         if(password1==conform_pass):
             
             if Teacher.objects.filter(email = Email).exists():
-                return HttpResponse(request,"Email already exists")
+                return HttpResponse("Email already exists")
             else:
                 user=Teacher.objects.create(email=Email,password=password1, department=departmentt)
                 user.save()
-                return HttpResponse(request,"created")
+                print("teacher created")
+                return render(request,"login.html")
         else:
-            return HttpResponse(request,"password not match")
+            return HttpResponse("password not match")
 
     else:
-        return HttpResponse(request,"mfnfnf")
+        return HttpResponse("mfnfnf")
+
+
+def login(request):
+    if request.method=='POST':
+        Email=request.POST['Email']
+        password=request.POST['password']
+    
+        Teacher_dir={}
+
+        for i in Teacher.objects.all():
+           Teacher_dir[i.email]=i.password
+           if(i.email==Email):
+               context={'obj':i}
+
+        if(Email in Teacher_dir and Teacher_dir[Email]==password):
+            
+            return render(request,'take_attendence.html',context)
+        else:
+            return HttpResponse("Invalid password or Email Id")
+
+    else:
+        return HttpResponse("fail")
+        
+    
