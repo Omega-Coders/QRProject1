bcc48dddf665cbdf7929172a949896262879cbbc
sundar.G <sundar.g@sundarGs-MacBook-Pro.local>
2022-04-09 16:18:24 +0530

    version-8

--- attendence/Scanner/views.py
+++ attendence/Scanner/views.py
@@ -37,82 +37,61 @@ def home(request):
     dip_list=[]
     for i in Department.objects.all():
         dip_list.append(i.deptName)
-
     context1={'obj':dip_list}
-    #period=[1, 2, 3, 4, 5, 6, 7, 8]
     section=['-','A','B','C','D']
-    #return render(request,'take_attendence.html',{'teacher':context,'department':context1,'period':period,'section':section})
     return render(request,'Scanner/index.html',{'department':context1,'section':section})
 def signup(request):
     dip_list=[]
     for i in Department.objects.all():
         dip_list.append(i.deptName)
-
-    context1={'obj':dip_list}
-    #period=[1, 2, 3, 4, 5, 6, 7, 8]
+    context1={'obj':dip_list} 
     section=['-','A','B','C','D']
     if request.method =="POST":
-        
-        #return render(request,'take_attendence.html',{'teacher':context,'department':context1,'period':period,'section':section})
-        #return render(request,'Scanner/index.html',{'department':context1,'section':section})
+        global username
         username=request.POST.get("username",False)
+        global email
+        
         email= request.POST.get('emailid',False)
+        global email1
+        email1=email
+        global pass1
         pass1 =request.POST.get('pass1',False)
         pass2 = request.POST.get("pass2",False)
+        global dept1
         dept1 =request.POST.get("dept",False)
+        global sec
         sec =request.POST.get("section",False)
-                #messages.success(request,"your are successfully logined")
         if(pass1 == pass2):
             if Student.objects.filter(user_name=username).exists():
                 messages.info(request,"username already exists")
-                return redirect("signup")
-                
+                return redirect("signup")      
             elif Student.objects.filter(emailid= email).exists():
                 messages.info(request,"Email already exists")
                 return redirect('signup')
             else:
-
-                myuser = Student.objects.create(user_name=username,emailid=email,password =pass1,department=dept1,section=sec)
-                myuser.is_active=False
-                #myuser1=myuser
-                myuser.save()
-                    #return redirect('signin') 
-                    #return redirect('signin')     
-                        
-                            #Student.objects.create(myuser.cleaned_data)
-                            
-                        #return HttpResponse(request,"created")
-                        
-                    #elif(Student.objects.filter(user_name=username).exists()==False):
-                        #myuser = Student.objects.create(user_name=username,emailid=email,password =pass1)
-                        #myuser.save()              
-                    
-                            #Student.objects.create(myuser.cleaned_data)
-                        #return redirect('signin')
-                        
-
-                #return HttpResponse(request,"created")
-                #welcome email
+                #myuser = Student.objects.create(user_name=username,emailid=email,password =pass1,department=dept1,section=sec)
+                #myuser.is_active=False
+                #myuser.save()
+                # sending an eamil msg to clent 
                 sub="welcome to Qr_attendence webpage"
-                msg="Hello"+" "+myuser.user_name+"!! \n"+"welocme to qr_attendence !! \n Thank you for visiting our website \n we have sent you confirmation mail ,please confirm your email to activate your Account .\n\n Thanking you \n Fantastic #4"
+                msg="Hello"+" "+username+"!! \n"+"welocme to qr_attendence !! \n Thank you for visiting our website \n we have sent you confirmation mail ,please confirm your email to activate your Account .\n\n Thanking you \n Fantastic #4"
                 from_email = settings.EMAIL_HOST_USER
-                to_list =[myuser.emailid]
+                to_list =[email]
                 send_mail(sub,msg,from_email,to_list,fail_silently=True)
                 #sending an email link
                 current_site = get_current_site(request)
                 email_subject = "Confirm your Email @ Qr-attendence  Login!!"
                 message2 = render_to_string('Scanner/email_confirmation.html',{
-                    
-                    'name': myuser.user_name,
+                    'name': username,
                     'domain': current_site.domain,
-                    'uid':  urlsafe_base64_encode(force_bytes(myuser.pk)),
-                    'token': generate_token .make_token(myuser)
+                    'uid':  urlsafe_base64_encode(force_bytes(email)),
+                    'token': generate_token .make_token(email)
                 })
                 email = EmailMessage(
                 email_subject,
                 message2,
                 settings.EMAIL_HOST_USER,
-                [myuser.emailid],
+                [email],
                 )
                 email.fail_silently = True
                 email.send()
@@ -120,36 +99,12 @@ def signup(request):
         else:
             messages.info(request,"password not match")
             return redirect('signup')
-    #return render(request,'Scanner/signup.html')
-    return render(request,'Scanner/signup.html',{'department':context1,'section':section})
-
-            #else:
-            #messages.info(request,"not rendering")
-        
-    
+    return render(request,'Scanner/signup.html',{'department':context1,'section':section})  
 def signin(request):
-    """if request.method=='POST':
-        email=request.POST.get("emailid")
-        password1= request.POST.get("pass")
-        user = authenticate(emailid=email,password=password1)
-        if user is not None:
-            login(request,user)
-            username1=user.emailid
-            messages.info(request,"succesfully logged in ")
-            return redirect("signin")
-        else:
-            messages.info(request,"invalid credentials!" )
-            return redirect("signin")
-    else:
-        messages.info(request,"not redirect")
-        #return redirect("signin")
-    return render(request,'Scanner/signin.html')"""
     if request.method=='POST':
         Email=request.POST.get('emailid')
         password=request.POST.get('pass')
-    
         Student_dir={}
-
         for i in Student.objects.all():
            Student_dir[i.emailid]=i.password
            if(i.emailid==Email):
@@ -157,19 +112,11 @@ def signin(request):
                global a 
                a= context
                print(i.emailid)
-
         if(Email in Student_dir and Student_dir[Email]==password):
-            #messages.info(request,"Successfullyloggedin")
             return redirect("scanner")
-
-            #return render(request,'take_attendence.html',context)
         else:
             messages.info(request,"Invalid password or Email Id")
             return redirect("signin")
-
-
-
-
     return render(request,'Scanner/signin.html')
 
 
@@ -178,27 +125,20 @@ def signin(request):
 def activate(request,uidb64,token):
     try:
         uid = urlsafe_base64_decode(uidb64).decode()
-        myuser = Student.objects.get(pk=uid)
+        username1=None
+        print(uid)
+        print(email)
+        if  uid is not None:   
+            username1 = uid
     except (TypeError,ValueError,OverflowError,Student.DoesNotExist):
-        myuser = None
-        #myuser=Student.objects.get(pk=uid)
-
-    if myuser is not None and generate_token.check_token(myuser,token):
-        myuser.is_active = True
-        # user.profile.signup_confirmation = True
-         ##return render(request,'Scanner/activation_failed.html')
-        #messages.info(request,myuser)
-
-        #myuser.is_active = True
-        #user.profile.signup_confirmation = True
-        #myuser.save()
-        #login(request,myuser)
+        username1 = None
+    if username1 is not None and generate_token.check_token(username1,token):
+        #username1.is_active = True
+        myuser = Student.objects.create(user_name=username,emailid=email1,password =pass1,department=dept1,section=sec)
         messages.info(request, "Your Account has been activated!!")
         return redirect('signin')
     else:
         return render(request,'Scanner/activation_failed.html')
-    
-# Create your views here.
 def scanner(request):
     return render(request,"Scanner/Scanner_pg1.html")
 def index(request):
@@ -206,24 +146,14 @@ def index(request):
 def gen(camera):
     while True:
         frame= camera.get_frame()
-        
         s,var=camera.get().read()
-        #decodedObjects = decode(var)
         detector = cv2.QRCodeDetector()
-    
-        
-
         if detector is not None:
             for code in  decode(var):
                 camera.getuse().append(code.data.decode('utf-8'))
                 if code.data.decode('utf-8') in camera.getuse():
-                    
-                    #myuser=Attendence.objects.create(qrinfo=code.data.decode('utf-8'))
-                    #myuser.save()
                     webbrowser.open(str(code.data.decode('utf-8')))
                     time.sleep(5)
-                    #return HttpResponse("successfully scanned")
-                    #messages.info(request,{{code.data.decode('utf-8')}})
                 else:
                     yield(b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
@@ -232,37 +162,24 @@ def gen(camera):
         """finally:
             yield(b'--frame\r\n'
             b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')"""
-
-
 def video_stream(request):
     dec=cv2.QRCodeDetector()
     return StreamingHttpResponse(gen(VideoCamera()),
                     content_type='multipart/x-mixed-replace; boundary=frame')
-    """else:
-        return redirect("signin")"""
 def link(request,date):
-
         per=date[8]
         dat=date[0:4]+"-"+date[4:6]+"-"+date[6:8]
         stu_sec=date[14:]
         stu_dep=date[9:14].replace("_","")
         e = str(a['obj'])
         con1={'date':dat,'period':per}
-
         si = Student.objects.filter(Q(section__in = [stu_sec])&Q(department__in=[stu_dep]))
-
         l = list(si)
-        
-        
         for i in l:
             if len(TakingAttendence.objects.filter(Q(date__in = [dat])& Q(deapartment_name__in =[stu_dep])&Q(reg__in= [i])& Q(section__in=[stu_sec]))) <1:
                 TakingAttendence.objects.create(date=dat, reg =i, deapartment_name=stu_dep, section=stu_sec, period_1="A",period_2="A", period_3="A", period_4="A", period_5="A", period_6="A", period_7="A", period_8="A")
-
-
         l2 = TakingAttendence.objects.filter(Q(date__in = [dat])& Q(deapartment_name__in =[stu_dep])&Q(reg__in= [e])& Q(section__in=[stu_sec]))
-        
         l3 = list(l2)
-        
         if per == "1":
             for i in l3:
                 i.period_1 = "P"
@@ -272,8 +189,7 @@ def link(request,date):
                 if(i.period_1 == "P"):
                     messages.info(request,"successfully taken")
             except:
-                messages.info(request,"please try again")
-                
+                messages.info(request,"please try again")        
         if per == "2":
             for i in l3:
                 i.period_2 = "P"
@@ -287,17 +203,13 @@ def link(request,date):
         if per == "3":
             for i in l3:
                 i.period_3 = "P"
-                i.save()
-               
-                
+                i.save()  
             abs3 = TakingAttendence.objects.filter(Q(date__in = [dat])& Q(deapartment_name__in =[stu_dep])&Q(reg__in= e)& Q(section__in=[stu_sec]) & Q(period_3__in=["A"]))
             try:
                 if(i.period_3 == "P"):
                     messages.info(request,"successfully taken")
             except:
                 messages.info(request,"please try again")
-            
-
         if per=="4":
             print('period-4')
             for i in l3:
@@ -350,9 +262,6 @@ def link(request,date):
                     messages.info(request,"successfully taken")
             except:
                 messages.info(request,"please try again")
-
-
-    
         return render(request,"Scanner/link.html",{"abc":a,"b":con1,"c":con1})
     
         #return HttpResponse("login first")
